eb546ce31ecca653047bf678e288e11363537fb4
Harshitha Sompura <sompharshi123@gmail.com>
2022-03-30 21:34:09 +0530

    Initial Commit

---
+++ TouristActivitiesApp_Group6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TouristActivitiesApp_Group6
+//
+//  Created by som on 30/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
